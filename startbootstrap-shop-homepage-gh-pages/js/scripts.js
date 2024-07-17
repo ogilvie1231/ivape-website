20ca26c64ff05685e5ea3f95a5b0c0edae7cad9e
ogilvie1231 <ogilvie1231@gmail.com>
2024-07-16 19:50:54 -0600

    setting up all pages and navbars

--- startbootstrap-shop-homepage-gh-pages/js/scripts.js
+++ startbootstrap-shop-homepage-gh-pages/js/scripts.js
@@ -1,7 +1,16 @@
 /*!
-* Start Bootstrap - Shop Homepage v5.0.6 (https://startbootstrap.com/template/shop-homepage)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
-*/
+ * Start Bootstrap - Shop Homepage v5.0.6 (https://startbootstrap.com/template/shop-homepage)
+ * Copyright 2013-2023 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
+ */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+document.getElementById("body").onscroll = function myFunction() {
+    var scrolltotop = document.scrollingElement.scrollTop;
+    var target = document.getElementById("main1");
+    var xvalue = "center";
+    var factor = 0.5;
+    var yvalue = scrolltotop * factor;
+    target.style.backgroundPosition = xvalue + " " + yvalue + "px";
+}
